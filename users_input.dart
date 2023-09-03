8a5f6ea9e3e9954dfe330e11a832146f54a0e0dd
apple mac <atfarhadi313@gmail.com>
2023-09-03 13:47:24 +0430

    Int user input

--- users_input.dart
+++ users_input.dart
@@ -4,4 +4,8 @@ void main() {
   print("Enter your name: ");
   String? name = stdin.readLineSync();
   print("The entered name is: ${name}");
+
+  print("Enter your age: ");
+  int? age = int.tryParse(stdin.readLineSync()!);
+  print("The Enter age is: ${age}");
 }
