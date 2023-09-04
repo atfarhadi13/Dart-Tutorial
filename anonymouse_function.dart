9628143e2e87460008e9a1b4ae6fbc841602f468
apple mac <atfarhadi313@gmail.com>
2023-09-04 12:52:54 +0430

    anonymouse function example

---
+++ anonymouse_function.dart
@@ -0,0 +1,13 @@
+void main() {
+  // const fruits = {"Apple", "Mango", "Orange", "Banana"};
+  // fruits.forEach((fruit) {
+  //   print(fruit);
+  // });
+
+  var cube = (int number) {
+    return number * number * number;
+  };
+
+  print("The cube of 2 is ${cube(2)}");
+  print("The cube of 3 is ${cube(3)}");
+}
